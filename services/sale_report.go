0eb4210f90ae91e0b32299cbe6c8c7d5080e29f7
Chavalee Jaroenpornvaranam <a677034@AR8030677034N1.local>
2024-06-24 17:18:27 +0700

    [wip] sale report

--- services/sale_report.go
+++ services/sale_report.go
@@ -6,10 +6,15 @@ import (
 	"math/rand"
 
 	"github.com/gin-gonic/gin"
+
+	"github.com/xuri/excelize/v2"
+
+	"fmt"
+	"time"
 )
 
 type MonthlySaleData struct {
-	Date        string `json:"date"`
+	Date        string `json:"date"` // "2021-02-01"
 	TotalAmount int    `json:"total_amount"`
 	SlipCount   []int  `json:"slip_count"`
 }
@@ -35,9 +40,11 @@ func GenSaleReport(c *gin.Context) {
 		result = append(result, base)
 	}
 
+	max := int(float32(base) * 2.5)
+
 	tempCount := 0
 	for remain > 0 {
-		randomNumber := rand.Intn(81) + 20
+		randomNumber := rand.Intn(81) + 25
 		randomNumber = randomNumber * 5
 		var randIndex int
 		if tempCount != -1 {
@@ -45,6 +52,11 @@ func GenSaleReport(c *gin.Context) {
 		} else {
 			randIndex = rand.Intn(slipTotal)
 		}
+
+		if result[randIndex] > max {
+			continue
+		}
+
 		if remain-randomNumber > 0 {
 			result[randIndex] = result[randIndex] + randomNumber
 		} else {
@@ -62,12 +74,51 @@ func GenSaleReport(c *gin.Context) {
 
 	println(result)
 
-	avg := int(saleData.TotalAmount) / slipTotal * 5
+	genExcel(result, saleData)
 
-	println(slipTotal, avg)
+	println(slipTotal)
 	c.JSON(http.StatusOK, gin.H{
 		"status":       "OK",
 		"date":         saleData.Date,
 		"total_amount": saleData.TotalAmount,
 	})
+}
+
+func genExcel(result []int, saleData MonthlySaleData) {
+	file := excelize.NewFile()
+	startDate, _ := time.Parse("2006-01-02", saleData.Date)
+	style, _ := file.NewStyle(&excelize.Style{NumFmt: 3})
+	headers := startDate
+
+	tempIdResult := 0
+	colNumber := 1
+
+	for i := 0; i < len(saleData.SlipCount); i++ {
+		colName := getColumnName(colNumber)
+		file.SetColWidth("Sheet1", colName, colName, 15)
+		file.SetCellValue("Sheet1", colName+"1", headers.Format("2006-01-02"))
+		itemPerDay := saleData.SlipCount[i]
+		rowNumber := 2
+		for i := 0; i < itemPerDay; i++ {
+			file.SetCellValue("Sheet1", colName+fmt.Sprint(rowNumber), result[tempIdResult])
+			rowNumber++
+			tempIdResult++
+		}
+		file.SetCellStyle("Sheet1", colName+"2", colName+fmt.Sprint(rowNumber), style)
+		colNumber++
+		headers = headers.AddDate(0, 0, 1)
+	}
+	file.SaveAs("sale_report.xlsx")
+}
+
+func getColumnName(col int) string {
+	name := make([]byte, 0, 3) // max 16,384 columns (2022)
+	const aLen = 'Z' - 'A' + 1 // alphabet length
+	for ; col > 0; col /= aLen + 1 {
+		name = append(name, byte('A'+(col-1)%aLen))
+	}
+	for i, j := 0, len(name)-1; i < j; i, j = i+1, j-1 {
+		name[i], name[j] = name[j], name[i]
+	}
+	return string(name)
 }

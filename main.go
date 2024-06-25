46989dcc8ede7de1dc5fcc9668875f86a62dd03b
Chavalee Jaroenpornvaranam <a677034@AR8030677034N1.local>
2024-06-25 15:22:45 +0700

    [fix] cors error

--- main.go
+++ main.go
@@ -6,10 +6,15 @@ import (
 	"pt-api/services"
 
 	"github.com/gin-gonic/gin"
+
+	"github.com/gin-contrib/cors" // Import CORS package
 )
 
 func main() {
 	r := gin.New()
+
+	// Apply CORS middleware
+	r.Use(cors.Default())
 
 	r.GET("/healthcheck", Healthcheck)
 	r.POST("/sale-report", services.GenSaleReport)
